c591af7a00d03068dff3fca5e7034d57976afc3d
Severino Adler <severino-adler@gmx.at>
2021-04-20 19:03:49 +0200

    added time-rev symmetry check

--- W2dynPostprocessing/symmetries.py
+++ W2dynPostprocessing/symmetries.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 
-def time_rev_symm_LS_matsubara(arr):
+def time_rev_symm_LS_iw(arr):
+
     """Symmetrise array acording to time reversal symmetry:
        s1,s2 = up/down (+1/-1 for numerical purposes)
        A_{a,s1,b,s2}(iw)=s1*s2*A_{a,s1bar,b,s2bar}^*(-iw)
@@ -24,6 +25,7 @@ def time_rev_symm_LS_matsubara(arr):
 
 
 def time_rev_symm_LS_tau(arr):
+
     """Symmetrise array acording to time reversal symmetry:
        s1,s2 = up/down (+1/-1 for numerical purposes)
        A_{a,s1,b,s2}(tau)=s1*s2*A_{a,s1bar,b,s2bar}^*(tau)
@@ -46,6 +48,7 @@ def time_rev_symm_LS_tau(arr):
 
 
 def time_rev_symm_LS_scalar(arr):
+
     """Symmetrise array acording to time reversal symmetry:
        s1,s2 = up/down (+1/-1 for numerical purposes)
        A_{a,s1,b,s2}=s1*s2*A_{a,s1bar,b,s2bar}^*
@@ -63,3 +66,17 @@ def time_rev_symm_LS_scalar(arr):
                                      arr[..., :, 0, :, 1].conjugate())
     newarr[..., :, 1, :, 1] = 0.5 * (arr[..., :, 1, :, 1] +
                                      arr[..., :, 0, :, 0].conjugate())
+
+
+def check_time_rev_symm_LS(arr, ax, **args):
+
+    if ax == 'scalar':
+        temparr = time_rev_symm_LS_scalar(arr)
+    elif ax == 'tau':
+        temparr = time_rev_symm_LS_tau(arr)
+    elif ax == 'iw':
+        temparr = time_rev_symm_LS_iw(arr)
+    else:
+        raise RuntimeError('ax={} not implemented'.format(ax))
+
+    return np.sum(np.abs(arr-temparr)), np.allclose(arr, temparr, **args)
